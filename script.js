3b5fd494a0ee3bf36f4190c4ba296e343259e38f
Fid <38692385+fidken@users.noreply.github.com>
2023-07-30 17:28:59 +0300

    Исправление ситуации

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-const http = require("http");
-const fs = requre("fs");
+const http = require('http');
+const fs = require('fs');
 
-const host = 'localhost';
-const port = 8010;
-
-const requestListener = function (req, res) {
-    fs.readFile('Untitled-1.html', (err, data) => {
-        if (err) {
-          // Ошибка чтения файла
-          res.writeHead(404);
-          res.write('File not found!');
-        } else {
-          // Отправка HTML-страницы
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.write(data);
-        }
-        res.end();
-      });
+const server = http.createServer((req, res) => {
+  // Чтение файла index.html
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      // Ошибка чтения файла
+      res.writeHead(404);
+      res.write('File not found!');
+    } else {
+      // Отправка HTML-страницы
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
     }
+    res.end();
+  });
+});
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
